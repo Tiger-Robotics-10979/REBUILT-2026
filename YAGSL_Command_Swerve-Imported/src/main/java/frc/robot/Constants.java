edd0c665c9b78fe688b68b3f5a68bf57f4626bd4
Ageezeetz <jose.hernandez-acevedo@crphs.org>
2026-02-10 18:32:19 -0500

    toggle for shooter

---
+++ YAGSL_Command_Swerve-Imported/src/main/java/frc/robot/Constants.java
@@ -0,0 +1,12 @@
+package frc.robot;
+
+public final class Constants {
+  public static class OperatorConstants {
+    public static final int kDriverControllerPort = 0;
+  }
+
+  public static class swerveDriveConstants {
+    public static final double MAX_VELOCITY = 3.0;
+    public static final double MAX_ANGULAR_VELOCITY = Math.PI;
+  }
+}
---
+++ YAGSL_Command_Swerve-Imported/src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -0,0 +1,45 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.math.controller.PIDController;
+
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class ShooterSubsystem extends SubsystemBase{
+    private final PIDController shooterPID = new PIDController(0, 0, 0);
+    public final SparkMax shooterMotor = new SparkMax(12, SparkMax.MotorType.kBrushless); //ID TBD 4
+
+    //private final CameraSubsystem m_camera;
+
+    
+    
+
+   
+    private double target; // = distanceFromTarget() * RPM; //this method will calculate the target rpm based on the distance from the target, which is calculated using the camera and some trigonometry.
+ 
+    public ShooterSubsystem(CameraSubsystem camera) {
+
+        //double setpoint = camera.getDistanceFromTarget() * 500.0; // RPM TO BE TESTED. USE EITHER THIS OR TARGET
+
+        final SparkMaxConfig shooterconfig = new SparkMaxConfig();
+        
+        shooterconfig.voltageCompensation(12);
+        shooterconfig.smartCurrentLimit(40);   
+
+        shooterMotor.configure(shooterconfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+
+    }
+
+    public void runShooter(double speed) { 
+        shooterMotor.set(speed);
+    }
+
+    public void stopShooter() {
+        shooterMotor.set(0); 
+    
+    }
+
+}
