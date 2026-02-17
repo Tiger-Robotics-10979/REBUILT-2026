4f8538be26ff63574e173712f41eb50e66521c4d
Ageezeetz <jose.hernandez-acevedo@crphs.org>
2026-02-17 17:56:53 -0500

    Started working on shooter encoder for PID

--- YAGSL_Command_Swerve-Imported/src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ YAGSL_Command_Swerve-Imported/src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -1,24 +1,23 @@
 package frc.robot.subsystems;
 
-import edu.wpi.first.math.controller.PIDController;
+// import edu.wpi.first.math.controller.PIDController;
 
+import com.revrobotics.RelativeEncoder;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkBase.PersistMode;
 import com.revrobotics.spark.SparkBase.ResetMode;
 import com.revrobotics.spark.config.SparkMaxConfig;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class ShooterSubsystem extends SubsystemBase{
-    private final PIDController shooterPID = new PIDController(0, 0, 0);
+public class ShooterSubsystem extends SubsystemBase {
+    // private final PIDController shooterPID = new PIDController(0, 0, 0);
     public final SparkMax shooterMotor = new SparkMax(12, SparkMax.MotorType.kBrushless); //ID TBD 4
 
-    //private final CameraSubsystem m_camera;
+    public final RelativeEncoder shooterEncoder = shooterMotor.getEncoder();
    
-    private double target; // = distanceFromTarget() * RPM; //this method will calculate the target rpm based on the distance from the target, which is calculated using the camera and some trigonometry.
- 
-    public ShooterSubsystem(CameraSubsystem camera) {
-
-        //double setpoint = camera.getDistanceFromTarget() * 500.0; // RPM TO BE TESTED. USE EITHER THIS OR TARGET
+    private double shooterRPM = shooterEncoder.getVelocity();
+    
+    public ShooterSubsystem() {
 
         final SparkMaxConfig shooterconfig = new SparkMaxConfig();
         
@@ -30,6 +29,8 @@ public class ShooterSubsystem extends SubsystemBase{
 
     public void runShooter(double speed) { 
         shooterMotor.set(speed);
+
+        System.out.println(shooterRPM);
     }
 
     public void stopShooter() {
